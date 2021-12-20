e036c5fab0fd1d1d1b98af7b70b0b6bc7375b9ed
freakeinstein <wow.dr.dre@gmail.com>
2021-12-20 14:28:32 +0530

    adding Aquila Search

--- search_server.py
+++ search_server.py
@@ -19,17 +19,45 @@ import rich
 import rich.markup
 import requests
 
+import os
+import random
+
+# get preset configs
+search_engine_ev = "google"
+search_api_key_ev = None
+
+# default url
+aquila_url_ev = "https://x.aquila.network/api/search"
+
+server_host_ev = "0.0.0.0"
+server_port_ev = 8080
+
+if os.getenv('SEARCH_ENGINE'):
+    search_engine_ev = os.getenv('SEARCH_ENGINE')
+    
+if search_engine_ev == "aquila":
+    search_api_key_ev = os.getenv('SEARCH_API_KEY')
+    if os.getenv('AQUILA_URL'):
+        aquila_url_ev = os.getenv('AQUILA_URL')
+
+if os.getenv('HOST'):
+    server_host_ev = os.getenv('HOST')
+
+if os.getenv('PORT'):
+    server_port_ev = os.getenv('PORT')
+
 
 print = rich.print
 
-_DEFAULT_HOST = "0.0.0.0"
-_DEFAULT_PORT = 8080
+_DEFAULT_HOST = server_host_ev
+_DEFAULT_PORT = server_port_ev
 _DELAY_SEARCH = 1.0  # Making this too low will get you IP banned
 _STYLE_GOOD = "[green]"
 _STYLE_SKIP = ""
 _CLOSE_STYLE_GOOD = "[/]" if _STYLE_GOOD else ""
 _CLOSE_STYLE_SKIP = "[/]" if _STYLE_SKIP else ""
 _REQUESTS_GET_TIMEOUT = 5
+
 
 def _parse_host(host: str) -> Tuple[str, int]:
     """ Parse the host string. 
@@ -184,6 +212,21 @@ class SearchABC(http.server.BaseHTTPRequestHandler):
         # Prepare the answer and send it
         ###############################################################
         content = content[:n]  
+        # pick_list = [
+        #     "Elon Reeve Musk FRS (born June 28, 1971) is an entrepreneur and business magnate. He is the founder, CEO and Chief Engineer at SpaceX; early-stage investor, CEO and Product Architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. With an estimated net worth of around US$270 billion as of December 2021,[2] Musk is the richest person in the world.",
+        #     "Musk was born to a Canadian mother and South African father, and raised in Pretoria, South Africa. He briefly attended the University of Pretoria before moving to Canada at age 17 to avoid conscription. He was enrolled at Queen's University and later transferred to the University of Pennsylvania two years later, where he received a bachelor's degree in economics and physics, then moved to California in 1995 to attend Stanford University but decided instead to pursue a business career, co-founding the web software company Zip2 with his brother Kimbal. The startup was acquired by Compaq for $307 million in 1999. The same year, Musk co-founded online bank X.com, which merged with Confinity in 2000 to form PayPal. The company was bought by eBay in 2002 for $1.5 billion.",
+        #     "In 2002, Musk founded SpaceX, an aerospace manufacturer and space transport services company, of which he is CEO and CTO. In 2004, he joined electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.) as chairman and product architect, becoming its CEO in 2008. In 2006, he helped create SolarCity, a solar energy services company that was later acquired by Tesla and became Tesla Energy. In 2015, he co-founded OpenAI, a nonprofit research company that promotes friendly artificial intelligence. In 2016, he co-founded Neuralink, a neurotechnology company focused on developing brain computer interfaces, and founded The Boring Company, a tunnel construction company. Musk has proposed the Hyperloop, a high-speed vactrain transportation system.",
+        #     "Musk has been the subject of criticism due to unorthodox or unscientific stances and highly publicized controversies. In 2018, he was sued for defamation by a British caver who advised in the Tham Luang cave rescue; a California jury ruled in favor of Musk. In the same year, he was sued by the US Securities and Exchange Commission (SEC) for falsely tweeting that he had secured funding for a private takeover of Tesla. He settled with the SEC, temporarily stepping down from his chairmanship and agreed to limitations on his Twitter usage. Musk has spread misinformation about the COVID-19 pandemic and has received criticism from experts for his other views on such matters as artificial intelligence, cryptocurrency, and public transport. "
+        # ]
+        # content = []
+        
+        # for itm in pick_list:
+        #     content.append({
+        #         'title': 'elon musk', 
+        #         'content': itm, 
+        #         'url': 'https://apple.com'
+        #     })
+        # print(random.choice(content)["content"])
         output = json.dumps(dict(response=content)).encode("utf-8")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -203,6 +246,22 @@ class GoogleSearchServer(SearchABC):
     def search(self, q: str, n: int) -> Generator[str, None, None]:
         return googlesearch.search(q, num=n, stop=None, pause=_DELAY_SEARCH)
 
+class FAISSSearchServer(SearchABC):
+    def search(self, q: str, n: int) -> Generator[str, None, None]:
+        payload = json.dumps({
+            "publicIndexId": search_api_key_ev,
+            "query": q
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", aquila_url_ev, headers=headers, data=payload)
+        resp_ = dict(sorted(response.json()['result'].items(), key=lambda item: item[1], reverse=True))
+        ret_ = list(resp_.keys())[:n]
+
+        return ret_
+
 
 class Application:
     def serve(
@@ -219,12 +278,24 @@ class Application:
         hostname, port = _parse_host(host)
         host = f"{hostname}:{port}"
 
-        with http.server.ThreadingHTTPServer(
-            (hostname, int(port)), GoogleSearchServer
-        ) as server:
-            print("Serving forever.")
-            print(f"Host: {host}")
-            server.serve_forever()
+        if search_engine_ev == "google":
+            print("Starting Google Search server..")
+            with http.server.ThreadingHTTPServer(
+                (hostname, int(port)), GoogleSearchServer
+            ) as server:
+                print("Serving forever.")
+                print(f"Host: {host}")
+                server.serve_forever()
+        elif search_engine_ev == "aquila":
+            print("Starting Aquila Search server..")
+            with http.server.ThreadingHTTPServer(
+                (hostname, int(port)), FAISSSearchServer
+            ) as server:
+                print("Serving forever.")
+                print(f"Host: {host}")
+                server.serve_forever()
+        else:
+            print("Choose a search server.")
 
     def test_parser(self, url: str) -> None:
         """ Test the webpage getter and parser. 
